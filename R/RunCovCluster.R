433424775272f991c6dab4996ff2db842da5de52
seasoncloud <yun820627@gmail.com>
2021-11-03 18:12:00 -0400

    multiple rounds

--- R/RunCovCluster.R
+++ R/RunCovCluster.R
@@ -7,15 +7,20 @@ RunCovCluster=function(mtx=NULL, barcodes=NULL, features=NULL, gtf=NULL, celltyp
 
   plot_path=paste0(dir_path,"/cov_hist.pdf")
   plot_path2=paste0(dir_path,"/cov_hist_updated.pdf")
+  result_all=list()
+
+  if(is.null(celltype0)){
+    celltype0=cbind(barcodes[,1], rep("normal", nrow(barcodes)))
+  }
 
   if( Est_read1==TRUE){# | !("deltas_allseg.rds" %in% list.files(paste0(dir_path,"rds")))){
     deltas_all=readRDS(paste0(dir_path, "/deltas_allseg.rds"))
-    }else{
-  deltas_all=EstRegionCov(mtx=mtx, barcodes=barcodes, features=features, gtf=gtf, celltype0=celltype0, var_pt=var_pt, var_pt_ctrl=var_pt_ctrl, include=include,
-                          alpha_source=alpha_source, ctrl_region=ctrl_region, seg_table_filtered=Obj_filtered$seg_table_filtered, size=size,
-                          plot_path=plot_path, breaks=breaks)
+  }else{
+    deltas_all=EstRegionCov(mtx=mtx, barcodes=barcodes, features=features, gtf=gtf, celltype0=celltype0, var_pt=var_pt, var_pt_ctrl=var_pt_ctrl, include=include,
+                            alpha_source=alpha_source, ctrl_region=ctrl_region, seg_table_filtered=Obj_filtered$seg_table_filtered, size=size,
+                            plot_path=plot_path, breaks=breaks)
 
-  saveRDS(deltas_all, paste0(dir_path, "/deltas_allseg.rds"))
+    saveRDS(deltas_all, paste0(dir_path, "/deltas_allseg.rds"))
   }
 
 
@@ -48,8 +53,8 @@ RunCovCluster=function(mtx=NULL, barcodes=NULL, features=NULL, gtf=NULL, celltyp
 
   }else{
 
-  clustering=BayesNonparCluster(Xir=df2, cna_states_WGS =cna_states_WGS , alpha=alpha, beta=beta, niter = niter , sigmas0 =  sigmas0, U0 = U0 , Z0 = Z0 , seed = seed)
-  saveRDS(clustering,paste0(dir_path, "/nonpara_clustering.rds"))
+    clustering=BayesNonparCluster(Xir=df2, cna_states_WGS =cna_states_WGS , alpha=alpha, beta=beta, niter = niter , sigmas0 =  sigmas0, U0 = U0 , Z0 = Z0 , seed = seed)
+    saveRDS(clustering,paste0(dir_path, "/nonpara_clustering.rds"))
   }
   clustering2=MCMCtrim(clustering)
 
@@ -59,23 +64,33 @@ RunCovCluster=function(mtx=NULL, barcodes=NULL, features=NULL, gtf=NULL, celltyp
 
 
   result1=list( deltas_all= deltas_all, cna_states_WGS=cna_states_WGS, df_obj=df_obj, clustering=clustering, clustering2=clustering2, result=result)
+  result_all[['result1']]=result1
 
   # update coverage estimation if needed
   result2=NULL
-  if(any(result$corrs<threshold_2nd)){
-    new_normal=rownames(clustering2$data)[which(result$corrs==unique(result$corrs)[which(unique(result$corrs)< threshold_2nd)])]
+  tmp=1
+  while(any(result$corrs< threshold_2nd)){
+    tmp=tmp+1
+    message("Estimated clusters with negative correlation to the WGS data.")
+    message(paste0("Start estimation ",tmp))
+
+    new_normal=rownames(clustering2$data)[which(result$corrs %in% unique(result$corrs)[which(unique(result$corrs)< threshold_2nd)])]
     celltype0[which(celltype0[,2]=='normal'),2]='normal_pre'
     celltype0[which(celltype0[,1] %in% new_normal),2]='normal'
 
-    if( Est_read2==TRUE){# | !("deltas_allseg.rds" %in% list.files(paste0(dir_path,"rds")))){
-      deltas_all=readRDS(paste0(dir_path, "/deltas_allseg_updated.rds"))
-    }else{
-    deltas_all=EstRegionCov(mtx=mtx, barcodes=barcodes, features=features, gtf=gtf, celltype0=celltype0, var_pt=var_pt, var_pt_ctrl=var_pt_ctrl, include=include,
-                            alpha_source=alpha_source, ctrl_region=ctrl_region, seg_table_filtered=Obj_filtered$seg_table_filtered, size=size,
-                            plot_path=plot_path2, breaks=breaks)
+    #   print(paste0('tmp:', tmp))
+    # print(paste0('Est_read2:', Est_read2))
+    # print(paste0('Clust_read2:', Clust_read2))
 
-    saveRDS(deltas_all, paste0(dir_path, "/deltas_allseg_updated.rds"))
-}
+    if( Est_read2==TRUE & paste0("deltas_allseg_updated",tmp,".rds") %in% list.files(dir_path)){# | !("deltas_allseg.rds" %in% list.files(paste0(dir_path,"rds")))){
+      deltas_all=readRDS(paste0(dir_path, "/deltas_allseg_updated",tmp,".rds"))
+    }else{
+      deltas_all=EstRegionCov(mtx=mtx, barcodes=barcodes, features=features, gtf=gtf, celltype0=celltype0, var_pt=var_pt, var_pt_ctrl=var_pt_ctrl, include=include,
+                              alpha_source=alpha_source, ctrl_region=ctrl_region, seg_table_filtered=Obj_filtered$seg_table_filtered, size=size,
+                              plot_path=plot_path2, breaks=breaks)
+
+      saveRDS(deltas_all, paste0(dir_path, "/deltas_allseg_updated",tmp,".rds"))
+    }
     df_obj=PrepCovMatrix(deltas_all=deltas_all, ngene_filter=ngene_filter)
     df=df_obj$df
     print(paste0("Dimension after filtering:",dim(df)))
@@ -99,14 +114,14 @@ RunCovCluster=function(mtx=NULL, barcodes=NULL, features=NULL, gtf=NULL, celltyp
       cna_states_WGS=clustering0$priors$cna_states_WGS[which(colnames(clustering0$data) %in% colnames(df2))]
     }
 
-    if( Clust_read2==TRUE){# | !("deltas_allseg.rds" %in% list.files(paste0(dir_path,"rds")))){
-      clustering=readRDS(paste0(dir_path, "/nonpara_clustering_updated.rds"))
+    if( Clust_read2==TRUE & paste0("nonpara_clustering_updated",tmp,".rds") %in% list.files(dir_path)){# | !("deltas_allseg.rds" %in% list.files(paste0(dir_path,"rds")))){
+      clustering=readRDS(paste0(dir_path, "/nonpara_clustering_updated",tmp,".rds"))
 
     }else{
 
 
-    clustering=BayesNonparCluster(Xir=df2, cna_states_WGS =cna_states_WGS , alpha=alpha, beta=beta, niter = niter , sigmas0 =  sigmas0, U0 = U0 , Z0 = Z0 , seed = seed)
-    saveRDS(clustering,paste0(dir_path, "/nonpara_clustering_updated.rds"))
+      clustering=BayesNonparCluster(Xir=df2, cna_states_WGS =cna_states_WGS , alpha=alpha, beta=beta, niter = niter , sigmas0 =  sigmas0, U0 = U0 , Z0 = Z0 , seed = seed)
+      saveRDS(clustering,paste0(dir_path, "/nonpara_clustering_updated",tmp,".rds"))
     }
     clustering2=MCMCtrim(clustering, burnin = burnin, thinning = thinning)
 
@@ -116,9 +131,13 @@ RunCovCluster=function(mtx=NULL, barcodes=NULL, features=NULL, gtf=NULL, celltyp
 
     print(paste0("Zest: ",table(Zest)))
 
-    result2=list( deltas_all= deltas_all, cna_states_WGS=cna_states_WGS, df_obj=df_obj, clustering=clustering, clustering2=clustering2, result=result)
+    result2=list( deltas_all= deltas_all, cna_states_WGS=cna_states_WGS, df_obj=df_obj, clustering=clustering, clustering2=clustering2, result=result, new_normal=new_normal)
+    result_all[[paste0('result',tmp)]]=result2
+
+    #Est_read2=FALSE
+    #Clust_read2=FALSE
   }
 
-
-  return(list(result1=result1, result2=result2))
+  result_all[['celltype0']]=celltype0
+  return(result_all)
 }
