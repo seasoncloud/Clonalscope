1ac01eb1267f5d3b50544f66391f61f523c16cff
seasoncloud <yun820627@gmail.com>
2021-11-16 16:03:43 -0500

    corr rm_extreme

--- R/AssignCluster.R
+++ R/AssignCluster.R
@@ -1,156 +1,158 @@
-AssignCluster=function(cluster_obj=NULL, mincell=20, cutoff=0.3, allele=FALSE, cna_states_WGS=NULL){
+AssignCluster=function(cluster_obj=NULL, mincell=20, cutoff=0.3, allele=FALSE, cna_states_WGS=NULL, rm_extreme=F){
 
   if(allele==FALSE){
-  ## set values
-  R=ncol(cluster_obj$priors$U0)
-  Zest=cluster_obj$results$Zall
-  Uall=cluster_obj$results$Uall
-  Xir=cluster_obj$data
-  N=nrow(Xir)
+    ## set values
+    R=ncol(cluster_obj$priors$U0)
+    Zest=cluster_obj$results$Zall
+    Uall=cluster_obj$results$Uall
+    Xir=cluster_obj$data
+    N=nrow(Xir)
 
-  #if(is.null(corr_region_ind)){
+    #if(is.null(corr_region_ind)){
     corr_region_ind=1:R
-  #}
-  #print(corr_region_ind)
+    #}
+    #print(corr_region_ind)
 
-  ## take majority vote
-  set.seed(100)
-  maj_vote=apply(Zest, 2, function(x) which.max(table(x)[as.character(1:max(as.numeric(cluster_obj$results$Zall)))]))
-  Zest=maj_vote
+    ## take majority vote
+    set.seed(100)
+    maj_vote=apply(Zest, 2, function(x) which.max(table(x)[as.character(1:max(as.numeric(cluster_obj$results$Zall)))]))
+    Zest=maj_vote
 
-  ## filter out clusters < mincells
-  ind_clusters=as.numeric(names(table(Zest))[which(table(Zest)>mincell)])
-  ind_cell=which(!Zest %in% ind_clusters)
-  Zest[ind_cell]=NA
+    ## filter out clusters < mincells
+    ind_clusters=as.numeric(names(table(Zest))[which(table(Zest)>mincell)])
+    ind_cell=which(!Zest %in% ind_clusters)
+    Zest[ind_cell]=NA
 
-  ## compute new means
-  Usub=matrix(nrow=nrow(Uall[[length(Uall)]]), ncol=R)
-  for(kk in ind_clusters){
-    meanX=colMeans(Xir[which(Zest==kk),, drop=F],  na.rm = T)
-    meanX[is.na(meanX)]=0
-    Usub[kk,]=meanX
-  }
+    ## compute new means
+    Usub=matrix(nrow=nrow(Uall[[length(Uall)]]), ncol=R)
+    for(kk in ind_clusters){
+      meanX=colMeans(Xir[which(Zest==kk),, drop=F],  na.rm = T)
+      meanX[is.na(meanX)]=0
+      Usub[kk,]=meanX
+    }
 
-  sigmas=sqrt(1/(N-length(ind_cell))*colSums((Xir[-ind_cell,]-Usub[Zest[-ind_cell],])^2, na.rm = T))
+    sigmas=sqrt(1/(N-length(ind_cell))*colSums((Xir[-ind_cell,]-Usub[Zest[-ind_cell],])^2, na.rm = T))
 
-  ## Reassign cells not in the current clusters to one of the current cluster
-  for(ii in ind_cell){
-    P=rep(NA, nrow(Usub))
-    P[ind_clusters]=apply(Usub[ind_clusters,, drop=F],1, function(x) sum(dnorm(as.numeric(Xir[ii,]), x, sigmas, log=T), na.rm = T))
-    P[ind_clusters]=P[ind_clusters]-max(P[ind_clusters], na.rm = T)
-    #Zest[ii]=which.max(P)
-    P[is.na(P)]=min(P, na.rm = T)-100
-    P=exp(P)
-    Zest[ii]=sample.int(nrow(Usub), 1, replace = F, prob=P)
-  }
+    ## Reassign cells not in the current clusters to one of the current cluster
+    for(ii in ind_cell){
+      P=rep(NA, nrow(Usub))
+      P[ind_clusters]=apply(Usub[ind_clusters,, drop=F],1, function(x) sum(dnorm(as.numeric(Xir[ii,]), x, sigmas, log=T), na.rm = T))
+      P[ind_clusters]=P[ind_clusters]-max(P[ind_clusters], na.rm = T)
+      #Zest[ii]=which.max(P)
+      P[is.na(P)]=min(P, na.rm = T)-100
+      P=exp(P)
+      Zest[ii]=sample.int(nrow(Usub), 1, replace = F, prob=P)
+    }
 
-  ## Recompute new means and variance
-  Usub=matrix(nrow=nrow(Uall[[length(Uall)]]), ncol=R)
-  for(kk in ind_clusters){
-    meanX=colMeans(Xir[which(Zest==kk),, drop=F],  na.rm = T)
-    meanX[is.na(meanX)]=0
-    Usub[kk,]=meanX
-  }
-  colnames(Usub)=paste0("R",1:ncol(Usub))
-  rownames(Usub)=paste0("Cluster", 1:nrow(Usub))
+    ## Recompute new means and variance
+    Usub=matrix(nrow=nrow(Uall[[length(Uall)]]), ncol=R)
+    for(kk in ind_clusters){
+      meanX=colMeans(Xir[which(Zest==kk),, drop=F],  na.rm = T)
+      meanX[is.na(meanX)]=0
+      Usub[kk,]=meanX
+    }
+    colnames(Usub)=paste0("R",1:ncol(Usub))
+    rownames(Usub)=paste0("Cluster", 1:nrow(Usub))
 
-  sigmas=sqrt(1/(N)*colSums((Xir-Usub[Zest,])^2, na.rm = T))
+    sigmas=sqrt(1/(N)*colSums((Xir-Usub[Zest,])^2, na.rm = T))
 
-  ## order clusters and classify normal/tumor cells
-  if(is.null(cna_states_WGS)){
-  priors=cluster_obj$priors$cna_states_WGS#cluster_obj$priors$U0[2,]
-  }else{
-  priors=cna_states_WGS
-}
-  if(all(priors==1)){
-    priors=rep(1.5, ncol(Usub))
-    #priors=Usub[as.numeric(names(table(Zest)))[which.max(table(Zest))],]
-  }
+    ## order clusters and classify normal/tumor cells
+    if(is.null(cna_states_WGS)){
+      priors=cluster_obj$priors$cna_states_WGS#cluster_obj$priors$U0[2,]
+    }else{
+      priors=cna_states_WGS
+    }
+    if(all(priors==1)){
+      priors=rep(1.5, ncol(Usub))
+      #priors=Usub[as.numeric(names(table(Zest)))[which.max(table(Zest))],]
+    }
 
-  mu=NULL
-  for(ii in 1:6){
-    mu_tmp=c(rep(0.5*ii,ii+1),c(0,( (1:(ii))/ii)))
-    mu=rbind(mu, matrix(mu_tmp, byrow=F, ncol=2))
-  }
-
-
+    mu=NULL
+    for(ii in 1:6){
+      mu_tmp=c(rep(0.5*ii,ii+1),c(0,( (1:(ii))/ii)))
+      mu=rbind(mu, matrix(mu_tmp, byrow=F, ncol=2))
+    }
 
 
 
-  # correlation
-  # while(sd(priors)==0){
-  #   priors=sample(c(0.001, -0.001), length(priors), replace = T)+priors
-  # }
 
-  #ind_cna=which(priors != 1)
-  Usub2=apply(Usub,2, function(x) pmin(x,1.5))
-  #Usub2=apply(Usub,2, function(x) pmax(x,0.5))
-  corrs=apply(Usub2[,corr_region_ind,drop=F], 1, function(x){
-  tmp=sort((as.numeric(x)-1)*(as.numeric(priors[corr_region_ind])-1))
-  if(length(tmp)==0){
-    return(NA)
-  }else{
-  #return(sum(tmp[2:(length(tmp)-1)]))
-    return(sum(tmp))}
-  }
-  )#/(sqrt(sum((as.numeric(x)-1)^2))*sqrt(sum((as.numeric(priors[])-1)^2))))
-  #cor(as.numeric(priors),x)) ###c
-  corrs[is.na(corrs)]=0
-  corrs=corrs/max(corrs)
 
-  #if(dist=='correlation'){
-  # if(sd(priors)==0){
-  #   Manhattan <- function(a, b) (sum(abs(a - b)))
-  #   corrs=apply(Usub, 1, function(x) Manhattan(as.numeric(priors),x))/length(priors)
-  # }else{
+    # correlation
+    # while(sd(priors)==0){
+    #   priors=sample(c(0.001, -0.001), length(priors), replace = T)+priors
+    # }
 
-  ## cross-product
-  # library(lsa)
-  # crossproduct=function(x){
-  #   if(all(is.na(x))==F){
-  #     cos=cosine(priors, x)
-  #     sin=sqrt(1-cos^2)
-  #     #abscp=sqrt(sum(x^2))*sqrt(sum(priors2^2))*sin/sqrt(sum(priors2^2))
-  #     x2=abs(x-priors)+1
-  #     abscp=sqrt(sum(x2^2))/sqrt(sum(1*R))*sin
-  #     # x2=x#/(sqrt(sum(x^2)))
-  #     #   return(sqrt(sum((crossn(as.numeric(priors2),x2)))^2))
-  #   }else{
-  #     return(100)
-  #   }}
-  #
-  # corrs=apply(Usub, 1, crossproduct)
-  # corrs[is.na(corrs)]=100
+    #ind_cna=which(priors != 1)
+    Usub2=apply(Usub,2, function(x) pmin(x,1.5))
+    #Usub2=apply(Usub,2, function(x) pmax(x,0.5))
+    corrs=apply(Usub2[,corr_region_ind,drop=F], 1, function(x){
+      tmp=sort((as.numeric(x)-1)*(as.numeric(priors[corr_region_ind])-1))
+      if(length(tmp)==0){
+        return(NA)
+      }else{
+        if(rm_extreme==T){
+          return(sum(tmp[2:(length(tmp)-1)]))
+        }else{
+          return(sum(tmp))}}
+    }
+    )#/(sqrt(sum((as.numeric(x)-1)^2))*sqrt(sum((as.numeric(priors[])-1)^2))))
+    #cor(as.numeric(priors),x)) ###c
+    corrs[is.na(corrs)]=0
+    corrs=corrs/max(corrs)
 
-  ## eucledian
- # corrs=apply(Usub, 1, function(x) sqrt(sum((priors-x)^2))/length(priors))
-  #corrs[is.na(corrs)]=0
-  #}
-  # }else{
-  #   Manhattan <- function(a, b) (sum(abs(a - b)))
-  #   corrs=apply(Usub, 1, function(x) Manhattan(as.numeric(priors),x))/length(priors)
-  # }
-  annot=as.numeric(corrs)
+    #if(dist=='correlation'){
+    # if(sd(priors)==0){
+    #   Manhattan <- function(a, b) (sum(abs(a - b)))
+    #   corrs=apply(Usub, 1, function(x) Manhattan(as.numeric(priors),x))/length(priors)
+    # }else{
 
-  if(cutoff=='km'){
-  km=kmeans(annot[which(annot!=100)],2)
-  cutoff=mean(km$centers)
-  }
+    ## cross-product
+    # library(lsa)
+    # crossproduct=function(x){
+    #   if(all(is.na(x))==F){
+    #     cos=cosine(priors, x)
+    #     sin=sqrt(1-cos^2)
+    #     #abscp=sqrt(sum(x^2))*sqrt(sum(priors2^2))*sin/sqrt(sum(priors2^2))
+    #     x2=abs(x-priors)+1
+    #     abscp=sqrt(sum(x2^2))/sqrt(sum(1*R))*sin
+    #     # x2=x#/(sqrt(sum(x^2)))
+    #     #   return(sqrt(sum((crossn(as.numeric(priors2),x2)))^2))
+    #   }else{
+    #     return(100)
+    #   }}
+    #
+    # corrs=apply(Usub, 1, crossproduct)
+    # corrs[is.na(corrs)]=100
 
-  annot[annot>cutoff]="T"
-  annot[annot!="T"]=paste0("N")
+    ## eucledian
+    # corrs=apply(Usub, 1, function(x) sqrt(sum((priors-x)^2))/length(priors))
+    #corrs[is.na(corrs)]=0
+    #}
+    # }else{
+    #   Manhattan <- function(a, b) (sum(abs(a - b)))
+    #   corrs=apply(Usub, 1, function(x) Manhattan(as.numeric(priors),x))/length(priors)
+    # }
+    annot=as.numeric(corrs)
 
-  ## label clusters with priors
-  # Zest0=Zest
-  # if(!is.null(rownames(cluster_obj$priors$U0))){
-  # tmp=1:max(Zest)
-  # tmp[1:nrow(cluster_obj$priors$U0)]=rownames(cluster_obj$priors$U0)
-  # Zest=tmp[Zest]
-  # }
+    if(cutoff=='km'){
+      km=kmeans(annot[which(annot!=100)],2)
+      cutoff=mean(km$centers)
+    }
 
-  message("Succeed!")
+    annot[annot>cutoff]="T"
+    annot[annot!="T"]=paste0("N")
 
-  return(list(Zest=Zest, corrs=corrs[Zest], annot=annot[Zest],Uest=Usub, sigmas_est=sigmas, Zmaj= maj_vote, cutoff=cutoff, U0=cluster_obj$priors$U0,  wU0=cluster_obj$priors$wU0))
+    ## label clusters with priors
+    # Zest0=Zest
+    # if(!is.null(rownames(cluster_obj$priors$U0))){
+    # tmp=1:max(Zest)
+    # tmp[1:nrow(cluster_obj$priors$U0)]=rownames(cluster_obj$priors$U0)
+    # Zest=tmp[Zest]
+    # }
+
+    message("Succeed!")
+
+    return(list(Zest=Zest, corrs=corrs[Zest], annot=annot[Zest],Uest=Usub, sigmas_est=sigmas, Zmaj= maj_vote, cutoff=cutoff, U0=cluster_obj$priors$U0,  wU0=cluster_obj$priors$wU0))
 
 
   }else{  ### allele
@@ -165,7 +167,7 @@ AssignCluster=function(cluster_obj=NULL, mincell=20, cutoff=0.3, allele=FALSE, c
     N=nrow(Xir_cov)
 
     #if(is.null(corr_region_ind)){
-      corr_region_ind=1:R
+    corr_region_ind=1:R
     #}
     ## take majority vote
     set.seed(100)
@@ -270,8 +272,21 @@ AssignCluster=function(cluster_obj=NULL, mincell=20, cutoff=0.3, allele=FALSE, c
     priorsc=mu[priors,1]
 
     #ind_cna=which(priors != 1)
-    corrs=apply(Usubc[,corr_region_ind,drop=F], 1, function(x)
-      sum((as.numeric(x)-1)*(as.numeric(priorsc[corr_region_ind])-1)))#/(sqrt(sum((as.numeric(x)-1)^2))*sqrt(sum((as.numeric(priors[])-1)^2))))
+    #corrs=apply(Usubc[,corr_region_ind,drop=F], 1, function(x)
+    #  sum((as.numeric(x)-1)*(as.numeric(priorsc[corr_region_ind])-1)))#/(sqrt(sum((as.numeric(x)-1)^2))*sqrt(sum((as.numeric(priors[])-1)^2))))
+
+    corrs=apply(Usub2[,corr_region_ind,drop=F], 1, function(x){
+      tmp=sort((as.numeric(x)-1)*(as.numeric(priorsc[corr_region_ind])-1))
+      if(length(tmp)==0){
+        return(NA)
+      }else{
+        if(rm_extreme==T){
+          return(sum(tmp[2:(length(tmp)-1)]))
+        }else{
+          return(sum(tmp))}}
+    }
+    )
+
     #cor(as.numeric(priors),x)) ###c
     corrs[is.na(corrs)]=0
     corrs=corrs/max(corrs)
